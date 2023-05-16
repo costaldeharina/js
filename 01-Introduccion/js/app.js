94973e11bad54d0f0f1bef11ad7f312c0ddc811c
costaldeharina <josias.jepl@gmail.com>
2023-05-15 21:31:40 -0600

    Add: Importancia del punto y coma

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,8 +1,9 @@
+console.log ('Hola');
+console.log('Mundo');
 
-const nombre = prompt('Cual es tu nombre?');
 
-// Toma el nombre y lo muestra en pantalla 
-document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo js uwu`;
-
-// Asigna el valor hacia la variable del producto
-const producto = 'Monitor de 24 pulgadas'
+function hola(){
+    console.log('ok');
+    console.log('Hola');
+    console.log('Mundo');
+}
