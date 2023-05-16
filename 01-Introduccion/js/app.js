0fd7b75ef4354376fa26c4fa35791b5d306b0cc8
costaldeharina <josias.jepl@gmail.com>
2023-05-15 21:22:17 -0600

    Add: Uso de comentarios

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,2 +1,8 @@
+
 const nombre = prompt('Cual es tu nombre?');
+
+// Toma el nombre y lo muestra en pantalla 
 document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo js uwu`;
+
+// Asigna el valor hacia la variable del producto
+const producto = 'Monitor de 24 pulgadas'
