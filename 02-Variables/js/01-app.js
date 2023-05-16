bcfb3fa38ae5d0d4e92be932b082501dc5102fac
costaldeharina <josias.jepl@gmail.com>
2023-05-15 21:43:26 -0600

    Add: Inicializar variables

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,38 @@
+// Inicializar variable con un valor
+var producto = "Monitor de 24 pulgadas";
+console.log(producto);
+
+// Las variables se pueden reasignar
+producto = 'Monitor de 19 pulgadas';
+console.log(producto);
+
+// Js es un lenguaje de tipo dinamico, no especifica tipo de dato
+producto = 20;
+console.log(producto);
+
+
+// Se puede inicializar sin valor y asignarlo despues
+var disponible;
+disponible = true;
+
+disponible = false;
+
+// Inicializar multiples variables 
+var categoria = 'Computadora';
+marca = 'Marca famosa';
+calificacion = 9;
+
+// Las variables no pueden iniciar con numeros  
+var 99dias;
+var dias99;
+
+var 01_;
+var _01;
+var _usuario;
+var __usuario;
+
+
+var nombreProducto;
+var nombre_producto;
+var NombreProducto;
+
