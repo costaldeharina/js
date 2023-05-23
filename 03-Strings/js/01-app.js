79d7d6bde21cb69aae875d111cc1987e7a74ac63
costaldeharina <josias.jepl@gmail.com>
2023-05-22 21:18:01 -0600

    Add: Variables Strings en JS

---
+++ 03-Strings/js/01-app.js
@@ -0,0 +1,8 @@
+const producto = "monitor 20 pulgadas\"";
+const producto2 = String('monitor de 24 pulgadas');
+
+const producto3 = new String('monitor de 69 pulgadas');
+
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
