8515570f6d5ad5e799688a7283ed2e7e45d41722
costaldeharina <josias.jepl@gmail.com>
2023-05-22 21:32:56 -0600

    Add: String methods: Concatenar

---
+++ 03-Strings/js/03-app.js
@@ -0,0 +1,11 @@
+const producto = 'monitor 20 pulgadas ';
+const precio = '30 amlocoins';
+
+// console.log(producto.concat(precio));
+// console.log(producto.concat('En descuento'));
+
+console.log( producto + " Con un precio de: " + precio);
+
+console.log("El producto " + producto + "tiene un precio de " + precio);
+
+console.log(`El producto ${producto} tiene un precio de $ ${precio}`);
