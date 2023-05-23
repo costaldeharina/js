97f7c7f1b24e8a0094e1dccd3bd1a932e662f874
costaldeharina <josias.jepl@gmail.com>
2023-05-22 21:25:06 -0600

    Add: String methods: lenght, indexof and include

---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,14 @@
+const producto = 'monitor 20 pulgadas';
+
+console.log(producto)
+
+// Conocer la cantidad de letras de la cadena de texto
+console.log(producto.length)
+
+
+
+//console.log(producto.indexOf('tablet'));
+
+console.log(producto.includes('tablet'));
+console.log(producto.includes('Monitor'));
+console.log(producto.includes('monitor'));
